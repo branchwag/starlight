53fb9e446a28a752bb849ff847609f8dd4fc99c5
LeAnne B <branchleannelmb@gmail.com>
2024-05-14 13:55:49 -0400

    gets sat lib working to display lat long

--- src/components/Tracker.jsx
+++ src/components/Tracker.jsx
@@ -1,8 +1,18 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import {
+  twoline2satrec,
+  propagate,
+  gstime,
+  eciToGeodetic,
+  degreesLat,
+  degreesLong,
+} from "satellite.js";
 
 function Tracker() {
   const [data, setData] = useState(null);
+  const [lat, setLat] = useState(null);
+  const [long, setLong] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -12,6 +22,21 @@ function Tracker() {
         );
         const responseData = response.data;
         setData(responseData);
+
+        if (responseData) {
+          const now = new Date();
+          const satrec = twoline2satrec(responseData.line1, responseData.line2);
+          const positionAndVelocity = propagate(satrec, now);
+          const positionEci = positionAndVelocity.position;
+          const gmst = gstime(now);
+          const geodeticCoords = eciToGeodetic(positionEci, gmst);
+          const latitude = degreesLat(geodeticCoords.latitude);
+          const longitude = degreesLong(geodeticCoords.longitude);
+          // console.log(latitude);
+          // console.log(longitude);
+          setLat(latitude);
+          setLong(longitude);
+        }
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -26,6 +51,9 @@ function Tracker() {
         <>
           <p>{data.line1}</p>
           <p>{data.line2}</p>
+          <p>
+            Latitude: ${lat}, Longitude: ${long}
+          </p>
         </>
       ) : (
         <p>Loading data...</p>
